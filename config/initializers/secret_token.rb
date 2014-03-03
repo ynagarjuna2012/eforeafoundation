ba370fa5e0240b645af76b7321e79362f9253e8b
Nagarjuna <ynagarjuna2012@gmail.com>
2014-03-04 00:34:15 +0530

    Beforebundling the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Services::Application.config.secret_token = '357d3bcbbcb0feed546cc9cbf717192ff55ee1c6512d340fc569a833de5614d89d31976a0c059bd319ab15c62291a2435b225fa874479743cb78d30c911c5c6e'
